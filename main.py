728da0811980df2525e77d34f234d4bba880debb
Krzysztof <kotinerus@gmail.com>
2023-05-03 14:30:04 +0200

    Diffrent argument parsing and JSON 1/2

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-import sys
+import json
 
-args = sys.argv
-if(len(args)!=3):
-    print("#Error 01# Wrong amount of arguments. Program requires exactly two.")
-else:
-    primary = args[1]
-    secondary = args[2]
-    print(primary, secondary)
+
+def readFile(file):
+    openedFile = open(file, 'r')
+    return openedFile.read()
+
+
+def isJson(JSON):
+    try:
+        json.loads(JSON)
+    except ValueError as e:
+        return False
+    return True
+
+
+firstArgument = input("Insert first file: ")
+secondArgument = input("Insert second file: ")
+
+# Extension of the first file
+dot = firstArgument.rfind('.')
+firstArg = firstArgument[dot + 1:]
+
+# Extension of the second file
+dot = secondArgument.rfind('.')
+secondArg = secondArgument[dot + 1:]
+
+match firstArg.lower():
+    case 'json':
+
+        if isJson(readFile(firstArgument)):
+            print("It works!")
+
+
+        else:
+            print("[Error 02] This file is not JSON file.")
+
+
+    case _:
+        print("[Error 01] Wrong extension. Program accept only .json")
+
+input("PRESS [ENTER TO LEAVE]")
