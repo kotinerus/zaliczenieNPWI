f7f3fefa2d04e481cd373412f0dd03d59896a0b8
Krzysztof <kotinerus@gmail.com>
2023-05-04 19:22:10 +0200

    XML 2/2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import xml.parsers.expat
-
+import xml.etree.ElementTree as ET
 import yaml
 from xml.dom import minidom
+import xmltodict
 
 
 def readFile(file):
@@ -69,6 +70,12 @@ match firstArg.lower():
             match secondArg.lower():
                 case 'json': justCopy(readFile(firstArgument),secondArgument)
                 case 'yaml' | 'yml' : JSONtoyml(firstArgument,secondArgument)
+                case 'xml':
+                    with open(firstArgument,'r') as jsonFile:
+                        data_dict = json.load(jsonFile)
+                        xml_string = xmltodict.unparse(data_dict)
+                        with open(secondArgument,'w') as xmlFile:
+                            xmlFile.write(xml_string)
                 case _: print("[Error 03] Wrong extension. Program accept only .json, .yml, .xml")
         else:
             print("[Error 02] This file is not JSON file.")
@@ -77,17 +84,40 @@ match firstArg.lower():
             match secondArg.lower():
                 case 'json': ymlToJSON(firstArgument,secondArgument)
                 case 'yaml' | 'yml' : justCopy(readFile(firstArgument),secondArgument)
+                case 'xml':
+                    with open(firstArgument, 'r') as yamlFile:
+                        data = yamlFile.read()
+                        data_dict = yaml.load(data, Loader=yaml.FullLoader)
+                        xml_string = xmltodict.unparse(data_dict)
+                        with open(secondArgument, 'w') as xmlFile:
+                            xmlFile.write(xml_string)
                 case _: print("[Error 03] Wrong extension. Program accept only .json, .yml, .xml")
         else:
             print("[Error 04] This file is not yaml file.")
     case 'xml':
         if isXML(firstArgument):
             match secondArg.lower():
-                case 'json': justCopy(readFile(firstArgument), secondArgument)
-                case 'yaml' | 'yml': JSONtoyml(firstArgument, secondArgument)
+                case 'json':
+                    with open(firstArgument) as xml:
+                        data_dict = xmltodict.parse(xml.read())
+                        json_data = json.dumps(data_dict)
+                        with open(secondArgument, "w") as json_file:
+                            json_file.write(json_data)
+
+                case 'yaml' | 'yml':
+                    with open(firstArgument) as xml:
+                        data_dict = xmltodict.parse(xml.read())
+                        yaml_data = yaml.dump(data_dict)
+                        with open(secondArgument, "w") as yaml_file:
+                            yaml_file.write(yaml_data)
+                case 'xml' :
+                    tree = ET.parse(firstArgument)
+                    root = tree.getroot()
+                    newTree = ET.ElementTree(root)
+                    newTree.write(secondArgument)
                 case _: print("[Error 03] Wrong extension. Program accept only .json, .yml, .xml")
         else:
-            print("[Error 05] This file is not yaml file.")
+            print("[Error 05] This file is not xml file.")
     case _:
         print("[Error 01] Wrong extension. Program accept only .json, .yml, .xml")
 
