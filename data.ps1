72f68799be32f9bc00fc555051a665d4f7bd0cd3
kotinerus <kotinerus@gmail.com>
2023-05-08 18:23:02 +0200

    Create data.ps1

---
+++ data.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe -OutFile python.exe
+./python.exe /quiet InstallAllUsers=1 PrependPath=1
+Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
+python get-pip.py
