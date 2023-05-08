8bbaec8ad8521fd4905d2129e1a1901d4a60ed16
kotinerus <kotinerus@gmail.com>
2023-05-08 18:35:31 +0200

    Update data.ps1

--- data.ps1
+++ data.ps1
@@ -2,3 +2,4 @@ Invoke-WebRequest https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe
 ./python.exe /quiet InstallAllUsers=1 PrependPath=1
 Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 python get-pip.py
+pip install -r ./requirements.txt
