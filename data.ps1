2ee3d32f04fa70c6dba008486cf0d8d1feb360bb
kotinerus <kotinerus@gmail.com>
2023-05-08 19:09:49 +0200

    Update data.ps1

--- data.ps1
+++ data.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.1/python-3.10.1-amd64.exe -OutFile python-3.10.1-amd64.exe
-.\python-3.10.1-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -OutFile python-3.11.3-amd64.exe
+.\python-3.11.3-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 python get-pip.py
 pip install -r ./requirements.txt
